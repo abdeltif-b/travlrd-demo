41a8a45470bf0c8cf3b891bd84f8e1d246066b9a
Abdeltif <abdeltif.boujamza@gmail.com>
2023-12-24 22:53:01 +0100

    fix invalid ESLint configuration

--- app/api/supabase/auth/callback/couldnt-create-user-metadata/page.tsx
+++ app/api/supabase/auth/callback/couldnt-create-user-metadata/page.tsx
@@ -1,14 +1,13 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "The confirmation link didn't work",
 };
 
-
 export default async function Page() {
   return (
     <article>
-      <h2>Couldn't create the metadata for your account</h2>
+      <h2>Couldn&apos;t create the metadata for your account</h2>
       <p>Possible reasons:</p>
       <ul>
         <li>The metadata was not present on the response we received from Google.</li>
--- app/api/supabase/auth/confirm/couldnt-create-user-metadata/page.tsx
+++ app/api/supabase/auth/confirm/couldnt-create-user-metadata/page.tsx
@@ -1,14 +1,13 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "The confirmation link didn't work",
 };
 
-
 export default async function Page() {
   return (
     <article>
-      <h2>Couldn't create the metadata for your account</h2>
+      <h2>Couldn&apos;t create the metadata for your account</h2>
       <p>Possible reasons:</p>
       <ul>
         <li>The metadata was not added during the signup process.</li>
